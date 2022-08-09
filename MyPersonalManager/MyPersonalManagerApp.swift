e0dbf22cf6258c7b1d2b61f314de3dc80e19637a
Tiago Pereira <tiago.fpereira@sage.com>
2022-08-09 15:38:46 +0200

    initial-commit

---
+++ MyPersonalManager/MyPersonalManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPersonalManagerApp.swift
+//  MyPersonalManager
+//
+//  Created by tiago.pereira on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MyPersonalManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
